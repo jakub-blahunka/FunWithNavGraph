908514bf5aecc237907372a5a9cdced32767537e
jaku8ka <jakub.blahunka@gmail.com>
2019-03-08 15:00:31 +0100

    Initial commit

---
+++ app/src/main/java/org/jaku8ka/fun/DefeatFragment.java
@@ -0,0 +1,29 @@
+package org.jaku8ka.fun;
+
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DefeatFragment extends Fragment {
+
+
+    public DefeatFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_defeat, container, false);
+    }
+
+}
---
+++ app/src/main/java/org/jaku8ka/fun/LoginFragment.java
@@ -0,0 +1,44 @@
+package org.jaku8ka.fun;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoginFragment extends Fragment {
+
+    Button loginBtn;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        loginBtn = view.findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_LoginFragment_to_RegisterFragment));
+    }
+}
+
