7c963d06301aca701e1426bfa2b12a51aaf1d41f
jaku8ka <jakub.blahunka@gmail.com>
2019-03-12 13:00:06 +0100

    win, defeat layout

--- app/src/main/java/org/jaku8ka/fun/DefeatFragment.java
+++ app/src/main/java/org/jaku8ka/fun/DefeatFragment.java
@@ -2,16 +2,29 @@ package org.jaku8ka.fun;
 
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DefeatFragment extends Fragment {
+
+    TextView tapToRetry;
+    ConstraintLayout tap;
 
 
     public DefeatFragment() {
@@ -26,4 +39,32 @@ public class DefeatFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_defeat, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tapToRetry = view.findViewById(R.id.tap_to_retry_TV);
+        tap = view.findViewById(R.id.tap_to_retry);
+
+        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.fadeout);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        tapToRetry.startAnimation(animation);
+
+        tap.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_DefeatFragment_to_GameFragment));
+    }
 }
--- app/src/main/java/org/jaku8ka/fun/WinFragment.java
+++ app/src/main/java/org/jaku8ka/fun/WinFragment.java
@@ -2,10 +2,19 @@ package org.jaku8ka.fun;
 
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 
 /**
@@ -13,6 +22,8 @@ import android.view.ViewGroup;
  */
 public class WinFragment extends Fragment {
 
+    TextView tapToHome;
+    ConstraintLayout tap;
 
     public WinFragment() {
         // Required empty public constructor
@@ -24,6 +35,35 @@ public class WinFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_win, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tapToHome = view.findViewById(R.id.tap_to_home_TV);
+        tap = view.findViewById(R.id.tap_to_retry);
+
+        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.fadeout);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        tapToHome.startAnimation(animation);
+
+        tap.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_WinFragment_to_HomeFragment));
     }
 
 }
